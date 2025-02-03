0e3bd26f25d0a6512d2d7c2cb9cfde758ce3f670
Sanjanakomire <sanjukmgoud11@gmail.com>
2025-02-03 13:21:37 +0530

    COMPLETED

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div className='container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20'>
-      <img width={200} src={assets.band}alt="" />
+      <img width={150} src={assets.band}alt="" />
       <p className='flex-1 border-1 border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @sk.dev | All right reserved.</p>
     <div className='flex gap-4'>
         <img width={38} src={assets.facebook_icon} alt="" />
